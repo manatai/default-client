63b59814f1806fb72cec765a0c59c247573c2931
Morozan Andrei <andreimorozantiberiu@gmail.com>
2021-03-11 20:09:33 +0200

    ADD - Components Structure LAYOUT

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # DefaultClient
 
 Created with Angular version 11.2.4.
+This is Angular default-client  , contain Angular UI packages , translate package , client/server API example . 
 
 ## Development packages
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConnectComponent } from './components/admin/connect/connect.component';
+import { DisconnectComponent } from './components/admin/disconnect/disconnect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectComponent,
+    DisconnectComponent
   ],
   imports: [
     BrowserModule,
