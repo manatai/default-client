4415cdb3d49fe24d25a319efa3fdd9fb0e288347
Morozan Andrei <andreimorozantiberiu@gmail.com>
2021-03-13 21:08:26 +0200

    UPDATE - Angular Material

--- src/app/components/ui/angular-material/ag-expansion-panel/ag-expansion-panel.component.ts
+++ src/app/components/ui/angular-material/ag-expansion-panel/ag-expansion-panel.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ViewChild, OnInit } from '@angular/core';
+import {MatAccordion} from '@angular/material/expansion';
 @Component({
   selector: 'app-ag-expansion-panel',
   templateUrl: './ag-expansion-panel.component.html',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgExpansionPanelComponent implements OnInit {
 
+  @ViewChild(MatAccordion) accordion!: MatAccordion;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
 
 }
--- src/app/components/ui/angular-material/ag-form-field/ag-form-field.component.ts
+++ src/app/components/ui/angular-material/ag-form-field/ag-form-field.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-ag-form-field',
@@ -10,6 +11,16 @@ export class AgFormFieldComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
 }
